15744db9f8643c72548f6b231ba18537e52337ec
realSenses <realSenses0@gmail.com>
2018-10-15 19:47:15 -0500

    style

---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Logo = props => (
+  <div className="game-logo">
+    <img src="https://media.giphy.com/media/swaXFSeBVHpHq/giphy.gif" alt="DYEG?" width="300px" className="logo"/>
+  </div>
+)
+
+export default Logo;
